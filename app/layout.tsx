60da6e5de61925fa7554b710d103fc333bda7bea
Mohak Kant <mohak@Mohaks-MacBook-Pro.local>
2023-03-17 10:51:59 -0700

    Initial MVP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import './globals.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css';
+
+import AppBar from '../components/AppBar';
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,8 +19,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" style={{ height: "100%" }}>
+      <body style={{ overflow: 'hidden' }}>
+        <AppBar />
+        <div style={{ height: "100%" }}>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
